131b255a249afcd541bb195e31481d09bd15ee5d
Martin Jacob Rehder <gitrepocon01@rehsco.com>
2021-05-09 15:43:41 +0200

    Initial Commit

---
+++ FlexMenu/FlexMenu.h
@@ -0,0 +1,18 @@
+//
+//  FlexMenu.h
+//  FlexMenu
+//
+//  Created by Martin Rehder on 09.05.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FlexMenu.
+FOUNDATION_EXPORT double FlexMenuVersionNumber;
+
+//! Project version string for FlexMenu.
+FOUNDATION_EXPORT const unsigned char FlexMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlexMenu/PublicHeader.h>
+
+
